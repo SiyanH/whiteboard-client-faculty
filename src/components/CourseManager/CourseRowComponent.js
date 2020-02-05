ea17efc6693dc2b73f62406f9e085facf4b4f63e
hsy <he.siya@husky.neu.edu>
2020-02-05 15:23:59 -0500

    Fix course row highlight (only highlight current selected row)

--- src/components/CourseManager/CourseRowComponent.js
+++ src/components/CourseManager/CourseRowComponent.js
@@ -2,12 +2,11 @@ import React from "react";
 
 class CourseRowComponent extends React.Component {
     state = {
-        clicked: false,
         editing: false,
         course: this.props.course
     };
 
-    setClicked = () => this.setState({clicked: true});
+    setClicked = () => this.props.setSelectedCourse(this.props.course);
     setEditing = () => this.setState({editing: true});
     setCourse = (e) => this.setState({
                                          course: {
@@ -23,10 +22,10 @@ class CourseRowComponent extends React.Component {
     render() {
         return (
             <ul className={`list-group list-group-horizontal wbdv-row wbdv-course 
-            ${this.state.clicked && "active"}`} onClick={this.setClicked}>
-                <li className={`list-group-item col-lg-6 col-10 ${this.state.clicked && "active"}`}>
+            ${this.props.isSelectedCourse && "active"}`} onClick={this.setClicked}>
+                <li className={`list-group-item col-lg-6 col-10 ${this.props.isSelectedCourse && "active"}`}>
                     <i className={`fas fa-file-alt wbdv-row wbdv-icon 
-                    ${this.state.clicked ? "text-white" : "text-primary"}`}></i>
+                    ${this.props.isSelectedCourse ? "text-white" : "text-primary"}`}></i>
                     {
                         !this.state.editing &&
                         <span className="ml-3 mr-3 wbdv-row wbdv-title"
@@ -41,26 +40,26 @@ class CourseRowComponent extends React.Component {
                     }
                 </li>
                 <li className={`list-group-item wbdv-row wbdv-owner col-lg-2 d-none d-lg-block 
-                ${this.state.clicked && "active"}`}>{this.props.course.owner}
+                ${this.props.isSelectedCourse && "active"}`}>{this.props.course.owner}
                 </li>
                 <li className={`list-group-item wbdv-row wbdv-modified-date col-lg-3 d-none d-lg-block
-                ${this.state.clicked && "active"}`}>{this.props.course.modifiedDate}
+                ${this.props.isSelectedCourse && "active"}`}>{this.props.course.modifiedDate}
                 </li>
                 <li className={`list-group-item d-flex justify-content-center col-lg-1 col-2
-                ${this.state.clicked && "active"}`} id="courseRowBtn">
+                ${this.props.isSelectedCourse && "active"}`} id="courseRowBtn">
                     {
-                        this.state.clicked && !this.state.editing &&
+                        this.props.isSelectedCourse && !this.state.editing &&
                         <i className="fas fa-pencil-alt text-white wbdv-row wbdv-button wbdv-edit"
                            role="button" title="Edit" onClick={this.setEditing}></i>
                     }
                     {
-                        this.state.clicked && !this.state.editing &&
+                        this.props.isSelectedCourse && !this.state.editing &&
                         <i className="fas fa-trash-alt text-white wbdv-row wbdv-button wbdv-delete"
                            role="button" title="Delete"
                            onClick={() => this.props.deleteCourse(this.props.course)}></i>
                     }
                     {
-                        this.state.clicked && this.state.editing &&
+                        this.props.isSelectedCourse && this.state.editing &&
                         <i className="fas fa-check text-white wbdv-row wbdv-button wbdv-update"
                            role="button" title="Update" onClick={this.updateCourse}></i>
                     }
--- src/components/CourseManager/CourseTableComponent.js
+++ src/components/CourseManager/CourseTableComponent.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import Course from "../../models/CourseModel";
 import CourseRowComponent from "./CourseRowComponent";
 
-const CourseTableComponent = ({showCourseEditor, deleteCourse, updateCourse, courses}) =>
-    <div className="container wbdv-course-list">
-        {
-            courses.map((course) =>
-                            <CourseRowComponent
-                                key={course._id}
-                                showCourseEditor={showCourseEditor}
-                                deleteCourse={deleteCourse}
-                                updateCourse={updateCourse}
-                                course={course}/>
-            )
-        }
-    </div>;
+const CourseTableComponent = ({showCourseEditor, deleteCourse, updateCourse, courses}) => {
+    const [selectedCourse, setSelectedCourse] = useState(new Course());
+
+    return (
+        <div className="container wbdv-course-list">
+            {
+                courses.map((course) =>
+                                <CourseRowComponent
+                                    key={course._id}
+                                    showCourseEditor={showCourseEditor}
+                                    deleteCourse={deleteCourse}
+                                    updateCourse={updateCourse}
+                                    course={course}
+                                    isSelectedCourse={course._id === selectedCourse._id}
+                                    setSelectedCourse={setSelectedCourse}/>
+                )
+            }
+        </div>
+    );
+};
 
 export default CourseTableComponent;
